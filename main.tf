edc0f0d49b02f3b38b6d4f38b0dab46ebc1054f0
Fabio S. de Oliveira <fabio.oliveira@cloudscript.com.br>
2024-07-29 15:03:01 -0300

    fix: update config module

--- main.tf
+++ main.tf
@@ -1,13 +1,17 @@
+
 provider "null" {}
 
 resource "null_resource" "kibana_action" {
+  count = length(var.actions)
+
   provisioner "local-exec" {
+    when    = create
     command = <<EOT
-      curl -X ${var.http_method} "${var.kibana_endpoint}/s/${var.kibana_space}/${var.kibana_api}" \
+      response=$(curl -X ${var.actions[count.index].http_method} "${var.kibana_endpoint}/s/${var.kibana_space}/${var.actions[count.index].kibana_api}" \
         -H "Content-Type: application/json" \
         -H "Authorization: ApiKey ${var.kibana_api_key}" \
         -H "kbn-xsrf: true" \
-        -d '${var.request_body}'
+        -d '${var.actions[count.index].request_body}')
     EOT
   }
 }
--- variables.tf
+++ variables.tf
@@ -14,17 +14,11 @@ variable "kibana_space" {
   type        = string
 }
 
-variable "kibana_api" {
-  description = "The specific API to call within the Kibana space (e.g., api/actions/connector)"
-  type        = string
-}
-
-variable "http_method" {
-  description = "The HTTP method to use for the request (e.g., POST, PUT)"
-  type        = string
-}
-
-variable "request_body" {
-  description = "The JSON body of the request"
-  type        = string
+variable "actions" {
+  description = "List of actions to be created in Kibana"
+  type = list(object({
+    http_method = string
+    kibana_api  = string
+    request_body = string
+  }))
 }

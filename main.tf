f867a8682b14c69df1ae9f023ba00b43693ef93f
Fabio S. de Oliveira <fabio.oliveira@cloudscript.com.br>
2024-08-09 02:03:10 -0300

    feat: update defaults values

--- main.tf
+++ main.tf
@@ -18,20 +18,20 @@ resource "elasticstack_kibana_alerting_rule" "alert" {
   count = var.create_alerts ? length(var.alerts) : 0
 
   name         = var.alerts[count.index].name
-  consumer     = var.alerts[count.index].consumer
-  rule_type_id = var.alerts[count.index].rule_type_id
-  interval     = var.alerts[count.index].interval
-  enabled      = var.alerts[count.index].enabled
-  notify_when  = var.alerts[count.index].notify_when
+  consumer     = merge(local.default_alert, var.alerts[count.index]).consumer
+  rule_type_id = merge(local.default_alert, var.alerts[count.index]).rule_type_id
+  interval     = merge(local.default_alert, var.alerts[count.index]).interval
+  enabled      = merge(local.default_alert, var.alerts[count.index]).enabled
+  notify_when  = merge(local.default_alert, var.alerts[count.index]).notify_when
 
-  params = jsonencode(var.alerts[count.index].params)
+  params = jsonencode(merge(local.default_params, var.alerts[count.index].params))
 
   dynamic "actions" {
     for_each = var.alerts[count.index].actions
     content {
       id    = actions.value.id
       group = actions.value.group
-      params = jsonencode(actions.value.params)
+      params = jsonencode(merge(local.default_action_params, actions.value.params))
     }
   }
 
